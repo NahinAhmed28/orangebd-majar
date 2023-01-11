62fac8861155186cb8be35a9ea48016f1690f922
nahin1 <nahin@mcc.com.bd>
2023-01-11 16:10:23 +0600

    seeder added

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 
 class AdminsTableSeeder extends Seeder
 {
@@ -14,6 +17,27 @@ class AdminsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Schema::disableForeignKeyConstraints();
+        DB::table('admins')->truncate();
+        DB::table('admins')->insert(array (
+            0 =>
+                array (
+//                    'id' => 1,
+                    'code' => 10,
+                    'username' => 'seed admin',
+                    'email' => 'admin'.'@gmail'.'.com',
+                    'title_en' => 'title_en',
+                    'title_bn' =>'title_bn',
+                    'contact' => 'contact',
+                    'address_en' => 'address_en',
+                    'address_bn' => 'address_bn',
+                    'center_id' => 'center_id',
+                    'usertype_id' =>1,
+                    'status' => 1,
+                    'password' => Hash::make('password'),
+                    'email_verified_at' => now(),
+                ),
+        ));
+        Schema::enableForeignKeyConstraints();
     }
 }
--- database/seeders/UserTypesTableSeeder.php
+++ database/seeders/UserTypesTableSeeder.php
@@ -20,77 +20,6 @@ class UserTypesTableSeeder extends Seeder
 
         DB::table('user_types')->truncate();
         DB::table('user_types')->insert(array(
-            // 0 =>
-            //     array(
-            //         'id' => 1,
-            //         'sort' => '1',
-            //         'code' => '1',
-            //         'name' => 'Master Type',
-            //         'title_en' => 'Master Admin',
-            //         'title_bn' => 'মাস্টার এডমিন',
-            //         'status' => 1,
-            //         'parent_id' => null,
-            //         'default_role' => 1,
-            //         'created_at' => NOW(),
-            //         'updated_at' => NOW(),
-            //     ),
-            // 1 =>
-            //     array(
-            //         'id' => 2,
-            //         'sort' => '2',
-            //         'code' => '2',
-            //         'name' => 'CCF',
-            //         'title_en' => 'CCF',
-            //         'title_bn' => 'সিসিএফ',
-            //         'status' => 1,
-            //         'parent_id' => null,
-            //         'default_role' => 2,
-            //         'created_at' => NOW(),
-            //         'updated_at' => NOW(),
-            //     ),
-            // 2 =>
-            //     array(
-            //         'id' => 3,
-            //         'sort' => '3',
-            //         'code' => '3',
-            //         'name' => 'DCCF',
-            //         'title_en' => 'DCCF',
-            //         'title_bn' => 'ডিসিসিএফ',
-            //         'status' => 1,
-            //         'parent_id' => null,
-            //         'default_role' => 2,
-            //         'created_at' => NOW(),
-            //         'updated_at' => NOW(),
-            //     ),
-            // 3 =>
-            //     array(
-            //         'id' => 4,
-            //         'sort' => '4',
-            //         'code' => '4',
-            //         'name' => 'CF',
-            //         'title_en' => 'CF',
-            //         'title_bn' => 'সিএফ',
-            //         'status' => 1,
-            //         'parent_id' => null,
-            //         'default_role' => 3,
-            //         'created_at' => NOW(),
-            //         'updated_at' => NOW(),
-            //     ),
-            // 4 =>
-            //     array(
-            //         'id' => 5,
-            //         'sort' => '5',
-            //         'code' => '5',
-            //         'name' => 'DCF',
-            //         'title_en' => 'DCF',
-            //         'title_bn' => 'ডিসিএফ',
-            //         'parent_id' => null,
-            //         'status' => 1,
-            //         'default_role' => 2,
-            //         'created_at' => NOW(),
-            //         'updated_at' => NOW(),
-            //     ),
-
 
             0 =>
                 array(
@@ -131,32 +60,32 @@ class UserTypesTableSeeder extends Seeder
                     'created_at' => NOW(),
                     'updated_at' => NOW(),
                 ),
-            // 3 =>
-            //     array(
-            //         'id' => 4,
-            //         'sort' => '4',
-            //         'code' => '4',
-            //         'name' => 'Manager',
-            //         'title_en' => 'Manager',
-            //         'title_bn' => 'ম্যানেজার',
-            //         'status' => 1,
-            //         'default_role' => 4,
-            //         'created_at' => NOW(),
-            //         'updated_at' => NOW(),
-            //     ),
-            // 4 =>
-            //     array(
-            //         'id' => 5,
-            //         'sort' => '5',
-            //         'code' => '5',
-            //         'name' => 'Operator',
-            //         'title_en' => 'Operator',
-            //         'title_bn' => 'অপারেটর',
-            //         'status' => 1,
-            //         'default_role' => 5,
-            //         'created_at' => NOW(),
-            //         'updated_at' => NOW(),
-            //     ),
+             3 =>
+                 array(
+                     'id' => 4,
+                     'sort' => '4',
+                     'code' => '4',
+                     'name' => 'Manager',
+                     'title_en' => 'Manager',
+                     'title_bn' => 'ম্যানেজার',
+                     'status' => 1,
+                     'default_role' => 4,
+                     'created_at' => NOW(),
+                     'updated_at' => NOW(),
+                 ),
+             4 =>
+                 array(
+                     'id' => 5,
+                     'sort' => '5',
+                     'code' => '5',
+                     'name' => 'Operator',
+                     'title_en' => 'Operator',
+                     'title_bn' => 'অপারেটর',
+                     'status' => 1,
+                     'default_role' => 5,
+                     'created_at' => NOW(),
+                     'updated_at' => NOW(),
+                 ),
         ));
 
 
