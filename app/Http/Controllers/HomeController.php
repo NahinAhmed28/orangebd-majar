f2c8feea58501385ba43ffdf21bc46f435b088d5
Nahin <nahin.ahmed28@gmail.com>
2023-01-14 15:02:48 +0600

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = [
+            'user' => User::auth()->user()->get(),
+            'pageTitle' => 'Murid Profile',
+        ];
+        return view('user.home', $data);
+
     }
 }
