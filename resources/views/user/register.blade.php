d130622b6de61d8779bc959b08cb6ae4a815a633
nahin1 <nahin@mcc.com.bd>
2023-01-15 11:24:33 +0600

    user register updated

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -62,7 +62,7 @@
                             <label for="image" class="col-md-4 col-form-label text-md-end">Image</label>
 
                             <div class="col-md-6">
-                                <input id="image" type="text" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" required autocomplete="image" autofocus>
+                                <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" required autocomplete="image" autofocus>
 
                                 @error('image')
                                 <span class="invalid-feedback" role="alert">
